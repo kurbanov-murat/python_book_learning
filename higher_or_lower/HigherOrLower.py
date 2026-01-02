dcb4ca72d29c214b58fd97a0af25ada88bef0ce0
Murat Kurbanov <mk4304146@gmail.com>
2026-01-02 15:44:35 +0300

    Реализовал базовую игровую логику Higher or Lower

--- higher_or_lower/HigherOrLower.py
+++ higher_or_lower/HigherOrLower.py
@@ -36,13 +36,52 @@ def draw_card(deck):
 # === ОСНОВНОЙ КОД ===
 def main():
     print('Welcome to Higher or Lower!')
+    print('Guess whether the next card will be higher or lower.')
+    print('+20 points for a correct guess, -15 for a wrong one.')
     print()
+    score = START_SCORE
 
     deck = create_deck()
     deck = shuffle_deck(deck)
+
     current_card = draw_card(deck)
     print(f"Starting Card: {current_card['rank']} of {current_card['suit']}")
+    print(f'Current score: {score}')
+    print()
 
+    for _ in range(N_CARDS):
+        answer = input(f"Will the next card be higher or lower than:"
+        f" {current_card['rank']} of {current_card['suit']} ?(h/l): ").strip().lower()
+
+        next_card = draw_card(deck)
+        print(f"Next Card: {next_card['rank']} of {next_card['suit']}")
+
+        if answer == 'h':
+            if next_card['value'] > current_card['value']:
+                print('Correct, It is higher.')
+                score += 20
+            else:
+                print('Wrong, It is not higher.')
+                score -= 15
+
+        elif answer == 'l':
+            if next_card['value'] < current_card['value']:
+                print('Correct, It is lower.')
+                score += 20
+            else:
+                print('Wrong, It is not lower.')
+                score -= 15
+
+        else:
+            print('Invalid input. No points awarded.')
+
+        print(f'Current score: {score}')
+        print()
+
+        current_card = next_card
+
+    print('Game Over!')
+    print(f'Final Score: {score}')
 
 if __name__ == "__main__":
     main()

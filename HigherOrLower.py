9572c94b24e59fa1e88ea4389f6702c2fd9d2642
Murat Kurbanov <mk4304146@gmail.com>
2026-01-02 11:31:29 +0300

    Добавил константы карт и начальные параметры игры

--- HigherOrLower.py
+++ HigherOrLower.py
@@ -1,6 +1,13 @@
 # Higher or Lower - Учебная консольная игра
 import random
 
+# === КОНСТАНТЫ КАРТ ===
+SUITS = ('Spades', 'Hearts', 'Clubs', 'Diamonds' )   # Пики, Червы, Трефы, Бубны
+RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+N_CARDS = 8
+START_SCORE = 50
+
+
 def main():
     print('Welcome to Higher or Lower!')
 

3ebc5861887b859fd7eb46581a4da938ef70158e
Murat Kurbanov <mk4304146@gmail.com>
2026-01-03 13:04:17 +0300

    Реализован класс Card с инкапсуляцией логики

--- higher_or_lower/game.py
+++ higher_or_lower/game.py
@@ -4,7 +4,21 @@
 
 class Card:
     """ Одна игральная карта """
-    pass
+
+    SUITS = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
+    RANKS = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+
+    def __init__(self, rank: str, suit: str):
+        self.rank = rank
+        self.suit = suit
+
+    @property
+    def value(self) -> int:
+        """Числовое значение карты (Ace = 1)"""
+        return Card.RANKS.index(self.rank) + 1
+
+    def __str__(self) -> str:
+        return f'{self.rank} of {self.suit}'
 
 
 class Deck:
